c8d9c71c6d01142b38a6b88a7f40c1f10f9bb5bb
olivier evalet <evaleto@gmail.com>
2017-06-12 11:14:40 +0200

    oe - npm export and typescript compilation (#12)
    * bootstrap helper for typescript and npm module publication
    
    * bootstrap helper for typescript and npm module publication

--- index.js
+++ index.js
@@ -1,30 +1,4 @@
-/**
- * Postfinance - main module
- * Copyright (c)2014, by Olivier Evalet <evaleto@gmail.com>
- * Copyright (c)2011, by Branko Vukelic <branko@herdhound.com>
- * Licensed under GPL license (see LICENSE)
- */
-
-var config = require('./lib/config');
-var check = require('./lib/check');
-
-module.exports = function(_conf) {
-	if(_conf){
-		config.configure(_conf);
-	}
-	var account = require('./lib/wallet');
-	var transaction = require('./lib/wallet.transaction');
-	var transfer = require('./lib/wallet.transfer');
-
-	var transaction=new transaction.Transaction();
-	return {
-		configure:config.configure,
-		mod10check:check.mod10check,
-		mod10gen:check.mod10gen,
-		option:config.option,
-		wallet:new account.Wallet(),
-		charge:transaction,
-		transaction:transaction,
-		transfer:new transfer.Transfer()
-	};
-};
+function __export(m) {
+    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
+}
+__export(require('./dist/wallet'));
